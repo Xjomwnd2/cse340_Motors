874953359893c3f061ff96fd7c4d183a61b76a78
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-18 01:09:57 +0300

    commit

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,6 +1,9 @@
 /* ****************************************
 *  Deliver login view
 * *************************************** */
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
 async function buildLogin(req, res, next) {
     let nav = await utilities.getNav()
     res.render("account/login", {
