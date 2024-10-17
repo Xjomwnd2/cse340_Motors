9b42afe164f847917b65b2d83f92a01074690f2c
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-17 18:19:01 +0300

    commit

--- server.js
+++ server.js
@@ -10,14 +10,15 @@ const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
+const cookieParser = require("cookie-parser");
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs") 
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root 
-
+app.set("view engine", "ejs"); 
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root 
+app.use(cookieParser())
 
 
 /* ***********************
