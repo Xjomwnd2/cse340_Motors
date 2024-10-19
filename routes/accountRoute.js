b2308be5f8c0f129466fe148f3baed2ceb47aa09
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-19 17:21:52 +0300

    commit

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/accountController');
-const regValidate = require('../utilities/registration-validation');
 const utilities = require('../utilities');
 
 // Route to deliver the login view (GET /login)
@@ -14,17 +13,12 @@ router.get('/login', (req, res, next) => {
 });
 
 // Process the login request (POST /login)
-router.post(
-  '/login',
-  utilities.checkLogin,
-  regValidate.checkLoginData,
-  (req, res, next) => {
-    try {
-      accountController.accountLogin(req, res);
-    } catch (error) {
-      next(error);
-    }
+router.post('/login', (req, res, next) => {
+  try {
+    accountController.accountLogin(req, res);
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 module.exports = router;
