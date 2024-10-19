adbdf331aa66c90ece53ed6c194d9ae6f9a1b2ca
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-19 11:37:31 +0300

    commit

---
+++ utilities/utilities.js
@@ -0,0 +1,18 @@
+// utilities.js
+function exampleUtilityFunction() {
+    return "This is a utility function!";
+  }
+  
+  module.exports = {
+    exampleUtilityFunction,
+  };
+  // utilities.js
+
+function getNav() {
+  // Your logic here
+}
+
+module.exports = {
+  getNav,
+  // other exports
+};
