b888d44ac91cd159fce28215769c7f84739ab9bd
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-19 16:54:53 +0300

    COMMIT

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,19 +1,25 @@
 // utilities.js
 
-// Example of handleErrors function
+// Define the handleErrors function
 const handleErrors = (controllerFunction) => {
-    return async (req, res, next) => {
-      try {
-        await controllerFunction(req, res, next);
-      } catch (error) {
-        next(error);
-      }
-    };
+  return async (req, res, next) => {
+    try {
+      await controllerFunction(req, res, next);
+    } catch (error) {
+      next(error); // Pass the error to the error handler middleware
+    }
   };
-  
-  // Export the function
-  module.exports = {
-    handleErrors,
-    // ... other exports
-  };
-  
+};
+
+// Define the checkLogin function
+const checkLogin = (req, res, next) => {
+  // Your login checking logic here
+  next();
+};
+
+// Export the functions
+module.exports = {
+  handleErrors,
+  checkLogin,
+  // Include any other utilities if needed
+};
